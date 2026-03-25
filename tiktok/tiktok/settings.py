2fb13dd8ebb3eab2d9a9cabfcce5bebcff3c0966
duckyduck01 <madhvetipathak2006@gmail.com>
2026-03-25 15:05:37 +0530

    Update settings.py

--- tiktok/tiktok/settings.py
+++ tiktok/tiktok/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&1st7_=yfj$m6-^ru%%n)r^g0f6)7fad^9%mxmpanr3!yrac6#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
