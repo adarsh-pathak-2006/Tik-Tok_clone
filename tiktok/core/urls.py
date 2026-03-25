522b516dbfc04a52cf486acfc2aff34677c004eb
duckyduck01 <madhvetipathak2006@gmail.com>
2026-03-25 17:10:41 +0530

    Delete tiktok directory

--- tiktok/core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('like/<int:video_id>/', views.like_video, name='like_video'),
-]
